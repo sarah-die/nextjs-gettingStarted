d118226e809a1eb8ba071fdcda0aa729d75da8bb
sarah-die <saridi@alice.de>
2023-05-08 14:42:38 +0200

    API routes

---
+++ pages/api/hello.js
@@ -0,0 +1,6 @@
+// req is an instance of http.IncomingMessage, plus some pre-built middlewares.
+// res is an instance of http.ServerResponse, plus some helper functions.
+
+export default function handler(req, res) {
+    res.status(200).json({ text: 'Hello' });
+}
